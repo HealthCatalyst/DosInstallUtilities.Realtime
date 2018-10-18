4d218e2d1cae64a5012300aed12497e0074762c6
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-10-17 20:53:29 -0700

    first try at sending HL7
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- functions/Testers/Test-SendingHL7.ps1
+++ functions/Testers/Test-SendingHL7.ps1
@@ -33,7 +33,7 @@ function Test-SendingHL7() {
 
     # from http://www.mieweb.com/wiki/Sample_HL7_Messages#ADT.5EA01
     [string] $message = @"
-    MSH|^~\&|SENDING_APPLICATION|SENDING_FACILITY|RECEIVING_APPLICATION|RECEIVING_FACILITY|20110613083617||ADT^A01|934576120110613083617|P|2.3||||
+MSH|^~\&|SENDING_APPLICATION|SENDING_FACILITY|RECEIVING_APPLICATION|RECEIVING_FACILITY|20110613083617||ADT^A01|934576120110613083617|P|2.3||||
 EVN|A01|20110613083617|||
 PID|1||135769||MOUSE^MICKEY^||19281118|M|||123 Main St.^^Lake Buena Vista^FL^32830||(407)939-1289^^^theMainMouse@disney.com|||||1719|99999999||||||||||||||||||||
 PV1|1|O|||||^^^^^^^^|^^^^^^^^
@@ -42,14 +42,20 @@ PV1|1|O|||||^^^^^^^^|^^^^^^^^
     $leadingCharacter = [char]11
     $endingCharacter1 = [char]28
     $endingCharacter2 = [char]13
-    [string] $command = "${leadingCharacter}${message}${endingCharacter1}${endingCharacter2}"
+    # [string] $command = "${leadingCharacter}${message}${endingCharacter1}${endingCharacter2}"
+    # [string] $command = $leadingCharacter + $message + $endingCharacter1 + $endingCharacter2
+    # [string] $command = [System.Convert]::ToChar(11) + $leadingCharacter + $message + [System.Convert]::ToChar(28) + [System.Convert]::ToChar(13)
+
+    [string] $command = "B" + $leadingCharacter + $message + $endingCharacter1 + $endingCharacter2
+
+    $data = [System.Text.ASCIIEncoding]::ASCII.GetBytes($command)
 
     [int] $port = 6661
 
     $tcpConnection = New-Object System.Net.Sockets.TcpClient($InterfaceEngineHost, $port)
     $tcpStream = $tcpConnection.GetStream()
     $reader = New-Object System.IO.StreamReader($tcpStream)
-    $writer = New-Object System.IO.StreamWriter($tcpStream)
+    $writer = New-Object System.IO.StreamWriter($tcpStream, [System.Text.ASCIIEncoding]::ASCII)
     $writer.AutoFlush = $true
 
     $buffer = new-object System.Byte[] 1024
@@ -64,20 +70,33 @@ PV1|1|O|||||^^^^^^^^|^^^^^^^^
 
     [DateTime] $startDate = Get-Date
     [int] $timeoutInMinutes = 1
+    [bool] $receivedResponse = $false
 
-    while ($tcpConnection.Connected -and ($startDate.AddMinutes($timeoutInMinutes) -gt (Get-Date))) {
+    while ($tcpConnection.Connected -and !$receivedResponse -and ($startDate.AddMinutes($timeoutInMinutes) -gt (Get-Date))) {
         while ($tcpStream.DataAvailable) {
             $rawresponse = $reader.Read($buffer, 0, 1024)
             $response = $encoding.GetString($buffer, 0, $rawresponse)
-            Write-Host "Response"
+            Write-Host "---- Response ---"
             Write-Host $response
+            Write-Host "---- End of Response ---"
+            $receivedResponse=$true
         }
 
         if ($tcpConnection.Connected) {
             if (!$messageSent) {
                 $messageSent = $true
-                Write-Host "Sending message"
-                $writer.WriteLine($command) | Out-Null
+                # [char[]] $array = @()
+                # $array += [char]11
+                # $array += $message.ToCharArray()
+                # $array += [char]28
+                # $array += [char]13
+                Write-Host "--- Sending message ---"
+                Write-Host $message
+                Write-Host "--- End of message ---"
+                Write-Host "--- Sending message ---"
+                Write-Host "0=[$($data[0])], 1=[$($data[1])]"
+                Write-Host "--- End of message ---"
+                $writer.Write($data, 0, $data.Length)
                 Write-Host "Waiting for response from $InterfaceEngineHost"
             }
         }
@@ -85,6 +104,8 @@ PV1|1|O|||||^^^^^^^^|^^^^^^^^
         Write-Host "." -NoNewline
         start-sleep -Milliseconds 500
     }
+
+    Write-Host ""
 
     $reader.Close()
     $writer.Close()
